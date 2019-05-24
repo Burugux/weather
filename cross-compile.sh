b18255cf54cff98b309b9e6f2d57149c1affbc83
leslie burugu <leslieburugu@gmail.com>
2019-05-24 16:37:24 +0300

    Testing deployments

---
+++ cross-compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+package="github.com/Burugux/weather"
+
+package_split=(${package//\// })
+package_name=${package_split[-1]}
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64" "darwin/386" "linux/386" "linux/amd64" "linux/arm" "linux/arm64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+    
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o cross/$output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
